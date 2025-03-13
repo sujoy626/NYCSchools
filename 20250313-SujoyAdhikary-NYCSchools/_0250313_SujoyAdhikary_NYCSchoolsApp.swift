393f18cbf0de3af9c3a10813f4417fb913db635b
Sujoy <sujoy626@gmail.com>
2025-03-13 17:23:45 +0530

    Initial Commit

---
+++ 20250313-SujoyAdhikary-NYCSchools/_0250313_SujoyAdhikary_NYCSchoolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  _0250313_SujoyAdhikary_NYCSchoolsApp.swift
+//  20250313-SujoyAdhikary-NYCSchools
+//
+//  Created by Sujoy Adhikary on 13/03/25.
+//
+
+import SwiftUI
+
+@main
+struct _0250313_SujoyAdhikary_NYCSchoolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 20250313-SujoyAdhikary-NYCSchoolsTests/_0250313_SujoyAdhikary_NYCSchoolsTests.swift
@@ -0,0 +1,17 @@
+//
+//  _0250313_SujoyAdhikary_NYCSchoolsTests.swift
+//  20250313-SujoyAdhikary-NYCSchoolsTests
+//
+//  Created by Sujoy Adhikary on 13/03/25.
+//
+
+import Testing
+@testable import _0250313_SujoyAdhikary_NYCSchools
+
+struct _0250313_SujoyAdhikary_NYCSchoolsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
